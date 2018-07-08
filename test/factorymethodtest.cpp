db87a2886b88b7ac65d9566202b7a96c08c8b1b0
Denis Keksel <denis.keksel@protonmail.com>
2018-07-08 23:38:53 +0200

    Fixed expect method

--- test/factorymethodtest.cpp
+++ test/factorymethodtest.cpp
@@ -13,8 +13,9 @@ namespace TestSuit {
 
     TEST_F(FactoryMethodTest,GetDegreeCoord){
         auto point = Point::NewPolar(5,M_PI_4);
-        EXPECT_EQ(point.x,3.53553);
-        EXPECT_EQ(point.y,3.53553);
+        auto result_x = 5*cos(M_PI_4);
+
+        EXPECT_FLOAT_EQ(point.x,result_x);
     }
 }
 
